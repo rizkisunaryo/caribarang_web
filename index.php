ade046d1b720462511b0612d0d7d3e0fa4e91554
Rizki Sunaryo <rizkisunaryo@gmail.com>
2016-02-06 05:18:58 +0700

    wip

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php 
 include_once 'constants.php';
+include_once 'inc/db.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>CARIBARANG</title>
+	<title>CariBarang</title>
 	<meta charset="utf-8"> 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" />
@@ -16,54 +17,50 @@ include_once 'constants.php';
 	<div class="container">
 		<div class="jumbotron">
 			<h1>caribarang</h1>
+			<p style="color:blue;">Mau belanja online? Cari aja di sini!</p>
 		</div>
 
 		<div class="row">
 			<div class="col-sm-3"></div>
 			<div class="col-sm-6">
-				<form role="form">
-				  	<div class="form-group">
-				    	<label for="keyword">Kata kunci:</label>
-				    	<input type="text" class="form-control" id="keyword">
-				  	</div>
-				  	<div class="form-group">
-					    <label for="min-price">Harga minimum:</label>
-				    	<input type="text" class="form-control" id="min-price">
-				  	</div>
-				  	<div class="form-group">
-					    <label for="max-price">Harga maksimum:</label>
-				    	<input type="text" class="form-control" id="max-price">
-				  	</div>
-				  	<button type="submit" class="btn btn-default">Cari</button>
+				<form role="form" method="GET">
+					<select class="form-control">
+						<option value="">=== Kategori ===</option>
+						<?php
+
+						$sql = "SELECT category,subcategory,label FROM category WHERE is_deleted=0 ORDER BY category,subcategory ";
+						$result = $conn->query($sql);
+
+						
+					    while($row = $result->fetch_assoc()) {
+					    	$value = $row['subcategory'];
+					    	$label = $row['label'];
+					    	if ($value=='') {
+					    		$value = $row['category'];
+					    	} else {
+					    		$label = '&nbsp;&nbsp;&nbsp;' . $label;
+					    	}
+					        // echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+					    ?>
+
+					    <option value="<?php echo $value; ?>"><?php echo $label; ?></option>
+
+					    <?php
+					    }						
+						$conn->close();
+
+						?>
+					</select><br />
+				    <input type="text" class="form-control" id="keyword" name="keyword" placeholder="Kata kunci"><br />
+				    <input type="tel" class="form-control" id="min-price" placeholder="Harga minimum"><br />
+				    <input type="tel" class="form-control" id="max-price" placeholder="Harga maximum"><br />
+				  	<button type="submit" class="btn btn-default">C A R I</button>
 				</form>
 			</div>
 			<div class="col-sm-3"></div>
-		</div>
-
-		 
+		</div>		 
 	</div>
-
-
-
-	<table>
-		<tr>
-			<td>Keyword</td>
-			<td>:</td>
-			<td><input type="text" id="form_keyword" /></td>
-		</tr>
-		<tr>
-			<td>Min price</td>
-			<td>:</td>
-			<td><input type="text" id="form_min-price" /></td>
-		</tr>
-		<tr>
-			<td>Max price</td>
-			<td>:</td>
-			<td><input type="text" id="form_max-price" /></td>
-		</tr>
-	</table>
-	<button id="frm_search">SEARCH</button>
-	<table id="result"></table>
+	
 	<script type="text/javascript">
 		$('#frm_search').click(function() {
 			$.ajax({
